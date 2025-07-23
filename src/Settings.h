dd064880bd415a9f903675cfbe2fe6b5463fb956
seouless <seouless724@gmail.com>
2025-07-23 18:14:00 +0200

    Added a settings file for future use

---
+++ src/Settings.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <Windows.h>
+#include <cwchar>
+
+namespace Globals::Settings
+{
+	inline std::size_t g_CountOfLdrLoadDll = 0;
+	inline HMODULE g_TGModule = nullptr;
+
+
+
+}
